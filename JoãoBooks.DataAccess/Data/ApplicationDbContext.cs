072531a85a66af20ff69d6af939d2ba31a15df11
JoaoVitorcan <W0779042@myscc.ca>
2022-11-02 12:21:04 -0300

    Modified namespace

--- JoãoBooks.DataAccess/Data/ApplicationDbContext.cs
+++ JoãoBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace JoãoBookStore.Data
+namespace JoãoBookStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
