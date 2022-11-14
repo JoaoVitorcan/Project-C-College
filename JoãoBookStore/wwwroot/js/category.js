7adbb2d75a5a94c8ffb094fb79e34bad4da077da
JoaoVitorcan <W0779042@myscc.ca>
2022-11-14 17:12:20 -0300

    category.js correction and implementation of the load table function outside the ready.function function

--- JoãoBookStore/wwwroot/js/category.js
+++ JoãoBookStore/wwwroot/js/category.js
@@ -1,18 +1,25 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    function loadDataTable() {
-        dataTable = $('#tblData').DataTable({
-            "ajax": {
-                "url": "/Admin/Category/GetAll"
-            },
-            "columns": [
-                { "data": "name", "width": "60%" },
-                {
-                    "data": "id",
-                    "reder": function (data) {
-                        return
-                        `
+    loadDataTable();
+
+});
+
+
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "reder": function (data) {
+                    return
+                    `
                             <div class="text-center">
                                 <a href="/Admin/Category/Upsert/$(data)" class="btn btn-success text-white" style="cursor:pointer">
                                     <i class="fas fa-edit"></i>&nbsp;
@@ -22,33 +29,32 @@ $(document).ready(function () {
                                 </a>
                             </div>
                                 `;
-                    }, "width": "40%"
-                }
-            ]
-        });
-    }
-    function Delete(url) {
-        swal({
-            title: "Are you sure you want to delete?",
-            text: "You will not be able to retore data!",
-            icon: "Warning",
-            buttons: true,
-            dangerMode: true
-        }).then(willDelete) => {
-            if (willDelete) {
-                $.ajax({
-                    type: "DELETE",
-                    url: url,
-                    success: function (data) {
-                        if (data.success) {
-                            toastr.success(data.message);
-                            dataTable.ajax.reload();
-                        } else {
-                            toastr.error(data.message);
-                        }
-                    }
-                });
+                }, "width": "40%"
             }
+        ]
+    });
+}
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to retore data!",
+        icon: "Warning",
+        buttons: true,
+        dangerMode: true
+    }).then(willDelete => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
         }
-    }
-});
+    });
+}
--- JoãoBooks.DataAccess/Repository/Repository.cs
+++ JoãoBooks.DataAccess/Repository/Repository.cs
@@ -9,54 +9,83 @@ using JoãoBookStore.DataAccess.Data;
 
 namespace JoãoBooks.DataAccess.Repository
 {
+    // Implements all the methods of the IRepository
     public class Repository<T> : IRepository<T> where T : class
     {
-        private readonly ApplicationDbContext _db;
+        // modify the database w/ the db context
+        private readonly ApplicationDbContext _db;      // get the db instance using the constructor and DI 
         internal DbSet<T> dbSet;
-
-        public Repository(ApplicationDbContext db)
+        public Repository(ApplicationDbContext db)     // use hot keys C-T-O-R to build the constructor
         {
             _db = db;
             this.dbSet = _db.Set<T>();
         }
-            
-
-
-
-
-        public void Add(T entify)
+        public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);      // add context so classes correspond to the DbSet in ApplicationDbContext
         }
 
         public T Get(int id)
         {
-            throw new NotImplementedException();
+            return dbSet.Find(id);
         }
 
         public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();      // returns the IEnumerable based on the conditions of the query
         }
 
         public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.FirstOrDefault();      // returns the IEnumerable based on the conditions of the query
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            T entity = dbSet.Find(id);
+            Remove(entity);
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entity)
         {
-            throw new NotImplementedException();
+            dbSet.RemoveRange(entity);
         }
     }
 }
